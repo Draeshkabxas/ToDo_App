933eccd710722af0a87162a02ea4029053abc8cb
I-TEECK <zzxxccz908@gmail.com>
2021-08-07 09:28:34 +0200

    Fix: make ADD floatingActionButton go to AddFragment.kt

--- app/src/main/java/com/derar/libya/todoapp/fragment/list/ListFragment.kt
+++ app/src/main/java/com/derar/libya/todoapp/fragment/list/ListFragment.kt
@@ -1,24 +1,34 @@
 package com.derar.libya.todoapp.fragment.list
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.derar.libya.todoapp.R
+import com.derar.libya.todoapp.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
 
 
-
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("Debug:findNav","Star Fragment")
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.bind(view)
+        binding.floatingActionButton.setOnClickListener { view ->
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+            Log.d("Debug:findNav","${view}")
+        }
+        return view
     }
 
 
