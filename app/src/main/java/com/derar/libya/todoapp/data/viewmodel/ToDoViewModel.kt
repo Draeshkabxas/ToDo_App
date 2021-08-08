8c797055a22c03092e8a0eb4a0b6ccc05c45f808
I-TEECK <zzxxccz908@gmail.com>
2021-08-08 06:09:18 +0200

    Add: add ToDoViewModel.kt and viewmodel package
    Add ToDoViewModel.kt for provide data to UI and survive configuration changes.
    it's actually a communication center between the Repository and the UI.

---
+++ app/src/main/java/com/derar/libya/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,34 @@
+package com.derar.libya.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.derar.libya.todoapp.data.ToDoDatabase
+import com.derar.libya.todoapp.data.models.ToDoData
+import com.derar.libya.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application)
+    :AndroidViewModel(application) {
+
+        private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+
+    private val repository: ToDoRepository
+
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+
+    init {
+        repository=ToDoRepository(toDoDao)
+        getAllData=repository.getAllData
+    }
+
+     fun insertData(toDoData: ToDoData){
+      viewModelScope.launch(IO) {
+          repository.insertData(toDoData)
+      }
+    }
+}
